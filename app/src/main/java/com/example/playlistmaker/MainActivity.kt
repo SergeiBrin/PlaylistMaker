859afa5e2f1d24700a40b0bde89c28a6741d14fe
Sergei <starodumovtmt@yandex.ru>
2024-05-15 03:21:09 +0500

    feat: add activities and screen transition

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -12,22 +13,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val searchButton = findViewById<Button>(R.id.search_button)
         searchButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку поиска!", Toast.LENGTH_SHORT).show()
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
 
         val mediaLibraryButton = findViewById<Button>(R.id.media_library_button)
         mediaLibraryButton.setOnClickListener {
-            Toast.makeText(this@MainActivity, "Нажали на кнопку медиатеки!", Toast.LENGTH_SHORT).show()
+            val mediaLibraryIntent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
         }
 
         val settingsButton = findViewById<Button>(R.id.settings_button)
         val settingsClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку настроек!", Toast.LENGTH_SHORT).show()
+                val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(settingsIntent)
             }
         }
         settingsButton.setOnClickListener(settingsClickListener)
