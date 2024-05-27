d8b51fff1c211eefb405279ea170292cb9f37fd2
Sergei <starodumovtmt@yandex.ru>
2024-05-27 20:17:59 +0500

    feat: implement the functionality of buttons on the settings screen

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +15,45 @@ class SettingsActivity : AppCompatActivity() {
         val arrowBackButton = findViewById<ImageView>(R.id.arrow_back)
         arrowBackButton.setOnClickListener {
             finish()
+        }
+
+        val actionShareAppButton = findViewById<FrameLayout>(R.id.btn_share_app)
+        actionShareAppButton.setOnClickListener {
+            val url = getString(R.string.uri_android_developer)
+
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, url)
+            }
+
+            startActivity(intent)
+        }
+
+        val buttonSupportContact = findViewById<FrameLayout>(R.id.btn_support_contact)
+        buttonSupportContact.setOnClickListener {
+            val email = getString(R.string.my_email)
+            val subject = getString(R.string.letter_subject)
+            val text = getString(R.string.letter_text)
+
+            val intent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
+
+            startActivity(intent)
+        }
+
+        val buttonUserAgreement = findViewById<FrameLayout>(R.id.btn_user_agreement)
+        buttonUserAgreement.setOnClickListener {
+            val urlOffer = getString(R.string.uri_practicum_offer)
+            val uri = Uri.parse(urlOffer)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+
+            startActivity(intent)
         }
     }
 }
