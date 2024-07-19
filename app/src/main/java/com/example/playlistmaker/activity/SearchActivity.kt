576b2b3ad21987403f0986b6c409876f231314b4
Sergei <starodumovtmt@yandex.ru>
2024-07-19 20:38:31 +0500

    style: Optimize imports

--- app/src/main/java/com/example/playlistmaker/activity/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/activity/SearchActivity.kt
@@ -25,8 +25,6 @@ import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
     private var inputText: String? = ""
