86ab0736b00062ddfc48688c6e9d57833d7ee7ac
Sergei <starodumovtmt@yandex.ru>
2025-02-15 03:39:57 +0500

    feat: Add a media library screen with fragments
    Fragments are implemented through Tablayout and ViewPager

--- app/src/main/java/com/example/playlistmaker/medialibrary/ui/activity/MediaLibraryActivity.kt
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/activity/MediaLibraryActivity.kt
@@ -2,11 +2,39 @@ package com.example.playlistmaker.medialibrary.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityMediaLibraryBinding
+import com.example.playlistmaker.medialibrary.ui.viewpager.MediaLibraryViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaLibraryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMediaLibraryBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_library)
+        binding = ActivityMediaLibraryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = MediaLibraryViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "Избранные треки"
+                1 -> tab.text = "Плейлисты"
+            }
+        }
+
+        tabMediator.attach()
+
+        binding.arrowBackHomeScreen.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
     }
 }
---
+++ app/src/main/java/com/example/playlistmaker/medialibrary/ui/viewpager/MediaLibraryViewPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker.medialibrary.ui.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.playlistmaker.medialibrary.ui.fragment.LikedSongsFragment
+import com.example.playlistmaker.medialibrary.ui.fragment.PlaylistsFragment
+
+class MediaLibraryViewPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(
+    fragmentManager,
+    lifecycle
+) {
+
+        override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> LikedSongsFragment.newInstance()
+            1 -> PlaylistsFragment.newInstance()
+            else -> throw IllegalArgumentException()
+        }
+    }
+}
